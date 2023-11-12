dc89ac77dc01edee142c51df234cf693efae4ac5
Luqas <118449875+epicreach@users.noreply.github.com>
2023-11-12 23:45:42 +0100

    created first scene draft
    -created first scene
    -added some css
    -javafx now works with maven

--- group3project/src/main/java/group3/Main.java
+++ group3project/src/main/java/group3/Main.java
@@ -1,7 +1,34 @@
 package group3;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("questionscene.fxml"));
+        Scene scene = new Scene(root);
+        String reusecss = this.getClass().getResource("smurfTitle.css").toExternalForm();
+        scene.getStylesheets().add(reusecss);
+        stage.setTitle("Smurfinator");
+        stage.setWidth(1280);
+        stage.setHeight(720);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
