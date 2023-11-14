8dbc9956e0a583e34381e7746335b91cfd93bb19
Luqas <118449875+epicreach@users.noreply.github.com>
2023-11-14 16:21:44 +0100

    delade upp klasserna och metoderna i olika filer
    funkar inte atm :(

--- group3project/src/main/java/group3/Main.java
+++ group3project/src/main/java/group3/Main.java
@@ -1,35 +1,20 @@
 package group3;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("questionscene.fxml"));
-        Scene scene = new Scene(root);
-        String titlecss = this.getClass().getResource("smurfTitle.css").toExternalForm();
-        String scenecss = this.getClass().getResource("sceneStyle.css").toExternalForm();
-        scene.getStylesheets().addAll(titlecss,scenecss);
-        stage.setTitle("Smurfinator");
-        stage.setWidth(1280);
-        stage.setHeight(720);
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        SmurfinatorMain sceneManager = new SmurfinatorMain(primaryStage);
+        String titlecss = getClass().getResource("styles/universalStyle.css").toExternalForm();
+        String scenecss = getClass().getResource("styles/sceneStyle.css").toExternalForm();
+        String[] stylesheetArray = {titlecss, scenecss};
+
+        sceneManager.loadAndShowScene("stages/questionscene.fxml", stylesheetArray);
     }
 
     public static void main(String[] args) {
         launch();
     }
-
 }
