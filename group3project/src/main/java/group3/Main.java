5ed8d6263492af34a7dbbbd036a9203348ee9a54
Huffla <dator343@gmail.com>
2023-11-15 16:41:58 +0100

    Tog bort onödig kod från main

--- group3project/src/main/java/group3/Main.java
+++ group3project/src/main/java/group3/Main.java
@@ -10,9 +10,9 @@ public class Main {
     static Model model;
     public static void main(String[] args) {
         model = new Model("Users.txt");
-        simpleTest();
+       
     }
     
-        // Just to test if objectoutputstream does anything, should be in JUnit tests but wanted a fast test.
+       
    
 }
