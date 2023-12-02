947afb4cb5cd3553fac0388f5b51b271e77db6b5
Jonatan Cederberg <jonceder@student.chalmers.se>
2023-12-02 01:48:42 +0100

    *SCHIZO VARNING* KOMMER JAG NUKEA MAIN NEXT???🥶🥶🥶

--- group3project/src/main/java/group3/modelFolder/CharacterFactory.java
+++ group3project/src/main/java/group3/modelFolder/CharacterFactory.java
@@ -8,36 +8,45 @@ public class CharacterFactory {
 
     public CharacterFactory(){
     }
-    private ArrayList<Trait> createTraitsFromStrings(Dictionary<String,Double> s){
 
-        ArrayList<Trait> traits = new ArrayList<>();
+    //Scans traitlist.txt to find the opposite traits of the trait with the name s
+    private ArrayList<String> createTraitOpposites(String s){
         File file = new File("traitlist.txt");
         Scanner scan;
         String line;
         String mainTrait = null;
         ArrayList<String> oppositeTraits = new ArrayList<>();
 
+        try {
+            scan = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (scan.hasNextLine()) {
+            line = scan.nextLine();
+            if(line.equals("STOP")){
+                assert mainTrait != null;
+                if(mainTrait.equals(s)) break;
+                mainTrait = null;
+                oppositeTraits = new ArrayList<>();
+            }
+            else if(mainTrait == null){
+                mainTrait = line;
+            }
+            else{
+                oppositeTraits.add(line);
+            }
+        }
+        return oppositeTraits;
+    }
+
+    //creates a trait from its name and a value
+    private ArrayList<Trait> createTraitsFromStrings(Dictionary<String,Double> s){
+
+        ArrayList<Trait> traits = new ArrayList<>();
         for(String name : Collections.list(s.keys())){
-            try {
-                scan = new Scanner(file);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            while (scan.hasNextLine()) {
-                line = scan.nextLine();
-                if(line.equals("STOP")){
-                    if(mainTrait != null && mainTrait.equals(name)) traits.add(new Trait(name, s.get(name), oppositeTraits));
-                    mainTrait = null;
-                    oppositeTraits = new ArrayList<>();
-                }
-                if(mainTrait == null){
-                    mainTrait = line;
-                }
-                else{
-                    oppositeTraits.add(line);
-                }
-            }
-            traits.add(new Trait(name, s.get(name), oppositeTraits));
+            createTraitOpposites(name);
+            traits.add(new Trait(name, s.get(name), createTraitOpposites(name)));
         }
         return traits;
     }
