02469d1e61f29d8abb0417d90e2c8f97fc3d9d33
Huffla <dator343@gmail.com>
2023-12-06 14:05:32 +0100

    Removed smurf.java and made not initialiez

--- group3project/src/main/java/group3/modelFolder/Model.java
+++ group3project/src/main/java/group3/modelFolder/Model.java
@@ -38,8 +38,8 @@ public class Model {
         ti.initialize();
         qi = new QuestionInitializer(questions_file_name);
         qi.initialize(); // initializes the question database with questions and their corresponding trait, remove if you do not want to create new each time
-        ci = new CharacterInitializer();
-        ci.intialize();
+        //ci = new CharacterInitializer();
+        //ci.intialize();
         user_list = user_handler.getUsers();
         traits_list = trait_handler.getTraits();
         character_list = character_handler.getCharacters();
