36595e615e1872af25b95dc18ebb3516afba8df6
Huffla <dator343@gmail.com>
2023-12-11 14:41:41 +0100

    Created the SceneTransitionHandler
    Now handles the things it should

--- group3project/src/main/java/group3/Main.java
+++ group3project/src/main/java/group3/Main.java
@@ -8,12 +8,15 @@ public class Main {
     static Model model;
     
     public static void main(String[] args) {
+        SceneTransitionHandler sceneTransitionHandler = SceneTransitionHandler.getInstance();
         model = Model.getInstance("Users.txt","Questions.txt","Traits.txt","Characters.txt");
         SmurfinatorMainController smurfinatorController = SmurfinatorMainController.getInstance(model.getSmurfinator());
         MainMenuController mainMenuController = MainMenuController.getInstance();
 
-        mainMenuController.setSmurfinatorController(smurfinatorController);
-        
+        //mainMenuController.setSmurfinatorController(smurfinatorController);
+        sceneTransitionHandler.setSmurfinatorController(smurfinatorController);
+        sceneTransitionHandler.setMainMenuController(mainMenuController);
+
         MainMenu.launchapp(args);
         
     }
